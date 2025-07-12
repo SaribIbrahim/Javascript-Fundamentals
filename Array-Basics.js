b2522d6f97f451707268f17ef330658f6f089a1f
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-12 20:54:31 +0500

    Worked on Rest and Spread Operator

--- Array-Basics.js
+++ Array-Basics.js
@@ -1,26 +1,30 @@
 
-let num=[1,2,4,5];
-//pushing new elements in the array
-let n=num;
-num.push(6,12,13);
-console.log(n);
-//n has same value as num because it is a reference to the same array so refernce is being copied not array
+
+// Pushing new elements in the array
+let num = [1, 2, 4, 5];
+let n = num;
+num.push(6, 12, 13);
+console.log(n); 
+// n has the same value as num because it is a reference to the same array.
+// Only the reference is copied — not the actual array.
 
 
-// now if we dont want to change the original array we can use spread operator
-let nums=[1,3,4,5];
-let n1=[...nums];// now n1 refers to a copy of nums not nums, its a new array
-nums.push(26,32,33);
-console.log(nums);
-console.log(n1);
-
-//now if want to copy only some indexes of org arr i.e destructuring
-let numss=[1,2,3,4,5,6,7,8,9,10];;
-let [elm1,elm2,elm3]=[...numss];
-console.log(elm1,elm2,elm3);
+// Now if we don't want to change the original array, we can use the spread operator
+let nums = [1, 3, 4, 5];
+let n1 = [...nums]; // Now n1 is a copy of nums, a new array (not the same reference)
+nums.push(26, 32, 33);
+console.log(nums);  // Modified original
+console.log(n1);    // Unchanged copy
 
 
+// Destructuring some elements from numss array e.g 0,1,2 index elements
+let numss = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let [elm1, elm2, elm3] = numss; // Destructuring first 3 elements
+console.log(elm1, elm2, elm3); // 1 2 3
 
 
-
+//using of rest operator while destructuring
+let array=[1,3,5,2,7,8];
+let [el1,el2,...remaining]=array;
+console.log(el1,el2,remaining);// 1 3 [5,2,7,8]
 
