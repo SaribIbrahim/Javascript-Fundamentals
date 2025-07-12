b44ad4b3ee4b258adde40a33504c60060c1e3e12
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-12 21:03:58 +0500

    Added a practice problem of spread and rest operator

--- Array-Basics.js
+++ Array-Basics.js
@@ -28,3 +28,31 @@ let array=[1,3,5,2,7,8];
 let [el1,el2,...remaining]=array;
 console.log(el1,el2,remaining);// 1 3 [5,2,7,8]
 
+
+// Practice Problem:
+// Push a new product: { name: "Cap", price: 600 } into the copied array.
+// Destructure the first two product objects from the copied array.
+// Print:
+// The original array
+// The copied array
+// The names of the first two products (destructured) — directly in console.log(), not stored in separate variables.
+
+
+let products = [
+    { name: "Shirt", price: 1200 },
+    { name: "Pant", price: 1800 },
+    { name: "Shoes", price: 2500 }
+  ];
+  
+  let prodCopy = [...products]; // shallow copy using spread
+  prodCopy.push({ name: "Cap", price: 600 }); // added new item
+  
+  let [element1, element2] = prodCopy; // destructuring first 2 items
+  
+  console.log(products);  // original (unchanged)
+  console.log(prodCopy);  // copied + new item
+  console.log(element1, element2); // printing destructured items directly
+  console.log(element1.name,element2.name); // printing destructured items names directly
+  
+  
+  
