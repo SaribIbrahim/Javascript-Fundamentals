c4971309aa208d224f9e99045c97ea3ecb4dc9da
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-13 20:18:27 +0500

    Added filter function & mix problem of map & filter

--- Loops.js
+++ Loops.js
@@ -21,8 +21,8 @@ let products = [
     { name: "Pant", price: 1500 },
   ];
 
-  let newProducts=products.map((item)=> {// call back returns a obj
-    return{// return also returns a obj
+  let newProducts=products.map((item)=> {
+    return{// call back function returns a obj
     ...item,// as obj is stored in reference so we need to spread it otherwise mutation will happen
     price:item.price*0.9}
   })
@@ -44,6 +44,84 @@ let products = [
   })
   console.log(students);
   console.log(newStudents);
+
+
+  //filter function
+  //filters elements from org array and returns a new array with elements that satisfy the condition
+  let numbers=[1,2,3,4,5,6,7,8,9,10];
+  let newNumbers=numbers.filter((num)=>{
+    return num>5;//condition
+  });
+  console.log(numbers);
+  console.log(newNumbers);
+
+  //practice problem of filter
+  let footballers=[{
+    name:"messi",
+    goals:150,
+    team:"barcelona"
+  },{
+    name:"ronaldo",
+    goals:140,
+    team:"real madrid"
+  },{
+    name:"neymar",
+    goals:110,
+    team:"psg"
+  },{
+    name:"kane",
+    goals:100,
+    team:"tottenham"
+  }
+];
+
+  let [elm1,...remaining]=footballers.filter((item)=>{
+    return item.goals>100;
+  });
+  console.log(footballers);
+  console.log(elm1,remaining);
+
+  //practice problem2 of filter
+  let players = [
+    { name: "Messi", goals: 150, assists: 60, team: "barcelona", position: "forward" },
+    { name: "Ronaldo", goals: 140, assists: 55, team: "real madrid", position: "forward" },
+    { name: "Neymar", goals: 110, assists: 70, team: "psg", position: "forward" },
+    { name: "Kane", goals: 100, assists: 30, team: "tottenham", position: "forward" },
+    { name: "Modric", goals: 50, assists: 80, team: "real madrid", position: "midfielder" },
+    { name: "Van Dijk", goals: 25, assists: 20, team: "liverpool", position: "defender" }
+  ];
+
+  let filteredPlayers=players.filter((items)=>{
+    if(items.goals>=100 && items.team!="tottenham" && items.position=="forward"){
+        return items;
+  }});
+  console.log(filteredPlayers);
+
+
+  // mix practice problem of map and filter
+  let mixplayers = [
+    { name: "Messi", goals: 150, assists: 60, team: "barcelona", position: "forward" },
+    { name: "Ronaldo", goals: 140, assists: 55, team: "real madrid", position: "forward" },
+    { name: "Neymar", goals: 110, assists: 45, team: "psg", position: "forward" },
+    { name: "Kane", goals: 100, assists: 30, team: "tottenham", position: "forward" },
+    { name: "Modric", goals: 50, assists: 80, team: "real madrid", position: "midfielder" },
+    { name: "Mbappe", goals: 130, assists: 60, team: "psg", position: "forward" },
+    { name: "Haaland", goals: 120, assists: 20, team: "man city", position: "forward" }
+  ];
+  let filteredMixplayers=mixplayers.filter(items=>
+    items.goals>=100 && items.assists>=50 && items.position=="forward"
+  );
+  let mappedMixPlayers=filteredMixplayers.map((items)=>{
+    return{
+        player:items.name,
+        team:items.team,
+        totalContribution:items.goals+items.assists
+    }
+  });
+  console.log(mappedMixPlayers);
+  
+  
+  
   
   
 
